a6743fee829feaaed9f5040d04230c02869555a3
Ranjan Thapa <98253643+sntiago1011@users.noreply.github.com>
2023-05-09 09:00:39 +0545

    completed template configue

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name='home')
+    path("home/", views.home, name='home'),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("services", views.services, name="services"),
 ]
