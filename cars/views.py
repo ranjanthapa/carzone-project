53373cef699789e049e906955abffe763fb96f79
Ranjan Thapa <98253643+sntiago1011@users.noreply.github.com>
2023-05-13 10:18:57 +0545

    Customization of admin pannel and search functionality

--- cars/views.py
+++ cars/views.py
@@ -8,8 +8,17 @@ def cars(request):
     paginator = Paginator(cars, 2)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
+    city_search = Car.objects.values('city').distinct()
+    year_search = Car.objects.values('year').distinct()
+    model_search = Car.objects.values('model').distinct()
+    body_style_search = Car.objects.values('body_style').distinct()
     data = {
         "cars": paged_cars,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'model_search': model_search,
+
     }
     return render(request, 'cars/cars.html', data)
 
@@ -20,3 +29,53 @@ def car_details(request, id):
         "car": car,
     }
     return render(request, 'cars/car_details.html', data)
+
+
+def search(request):
+    cars = Car.objects.order_by("-created_date")
+    city_search = Car.objects.values('city').distinct()
+    year_search = Car.objects.values('year').distinct()
+    model_search = Car.objects.values('model').distinct()
+    body_style_search = Car.objects.values('body_style').distinct()
+    transmission_search = Car.objects.values('transmission').distinct()
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.order_by('-created_date').filter(description__icontains=keyword)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        cars = cars.order_by('-created_date').filter(year__iexact=year)
+
+    if 'model' in request.GET:
+        model = request.GET['model']
+        cars = cars.order_by('-created_date').filter(model__iexact=model)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        cars = cars.order_by('-created_date').filter(city__iexact=city)
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        cars = cars.order_by('-created_date').filter(body_style__iexact=body_style)
+
+    if 'transmission' in request.GET:
+        transmission = request.GET['transmission']
+        cars = cars.order_by('-created_date').filter(body_style__iexact=transmission)
+
+    if 'min_price' and 'max_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        if max_price:
+            cars = cars.order_by('-created_date').filter(price__gte=min_price, price__lte=max_price)
+
+    data = {
+        'cars': cars,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'model_search': model_search,
+        'transmission_search': transmission_search,
+    }
+    return render(request, 'pages/search.html', data)
--- pages/views.py
+++ pages/views.py
@@ -7,10 +7,19 @@ def home(request):
     teams = Team.objects.all()
     featured_car = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by("-created_date")
+    city_search = Car.objects.values('city').distinct()
+    year_search = Car.objects.values('year').distinct()
+    model_search = Car.objects.values('model').distinct()
+    body_style_search = Car.objects.values('body_style').distinct()
+
     data = {
         "teams": teams,
         'featured_car': featured_car,
         'all_cars': all_cars,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'model_search': model_search,
     }
     return render(request, "pages/home.html", data)
 
